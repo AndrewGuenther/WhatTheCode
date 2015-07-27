8e2b37c6be98cd97921173aca1c2cabf7ccfaa04
Andrew Guenther <guenther.andrew.j@gmail.com>
2015-07-26 17:12:26 -0700

    Removed compiled files and updated gitignore

--- public/js/exports.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  window.WTC = require('wtc');
-
-  window.onload = function() {
-    return React.render(React.createElement(WTC.component.CodeBox, APP_PROPS), document.getElementById("content"));
-  };
-
-}).call(this);
